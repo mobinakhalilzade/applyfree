3cd20087c346659b7c9fe14ccdfe903d95f811cb
Mobina Khalilzadeh <mobinakhalilzadehs@outlook.com>
2020-10-11 20:09:31 +0330

    changes in filter div in program list, some styles

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  show: boolean = true;
-
+  sidebar: boolean; 
 
   constructor() { }
 
