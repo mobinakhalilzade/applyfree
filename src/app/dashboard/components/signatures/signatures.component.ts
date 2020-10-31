2983d9ace818d22eecad501b3c219712d5c29389
Mobina Khalilzadeh <mobinakhalilzadehs@outlook.com>
2020-10-31 15:36:08 +0330

    modify login register and signiture

--- src/app/dashboard/components/signatures/signatures.component.ts
+++ src/app/dashboard/components/signatures/signatures.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DashboardService } from '../../dashboard.service';
 import { Router } from '@angular/router';
 import { urls } from 'src/app/config/urls';
+import { UploadService } from '../../../helper/upload.service';
 
 @Component({
   selector: 'app-signatures',
@@ -9,6 +10,12 @@ import { urls } from 'src/app/config/urls';
   styleUrls: ['./signatures.component.css']
 })
 export class SignaturesComponent implements OnInit {
+  @ViewChild("fileUpload", { static: false })
+  fileUpload: ElementRef;
+  file = {};
+  form = {
+    image: null
+  }
   readonly stateEnum: typeof urls = urls;
 
   loading: boolean = true;
@@ -17,10 +24,37 @@ export class SignaturesComponent implements OnInit {
   alert: any;
   inlineModal: boolean;
   selectedId: any;
+  successUpload:boolean;
+  addSignature: boolean;
 
   urls = urls;
 
-  constructor(private service: DashboardService, public router: Router) { }
+  constructor(private service: DashboardService,
+    public router: Router,
+    private uploadService: UploadService
+  ) { }
+
+  upload() {
+    const fileUpload = this.fileUpload.nativeElement;
+    fileUpload.onchange = () => {
+      this.loading = true;
+      this.file = { data: fileUpload.files[0], inProgress: false, progress: 0 };
+      this.fileUpload.nativeElement.value = '';
+      this.uploadService.uploader(this.file, 'signature/add', (response: any) => {
+        if (response !== undefined) {
+          this.loading = false;
+          const body = response.body;
+          if (body.return == 200) {
+            this.alert = body.message;
+            this.signatures();
+          }
+          if (body.return == 300) {
+            this.alert = body.message;
+          }
+        }
+      })
+    }
+  }
 
   signatures() {
     const params = {
