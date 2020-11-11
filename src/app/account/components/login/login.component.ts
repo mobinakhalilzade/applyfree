a8e598f5d81c1ac3b53ce03b56325819dffc5e44
Maher Ashori <maher.ashori@hotmail.com>
2020-11-11 10:43:17 +0330

    login responsive

--- src/app/account/components/login/login.component.ts
+++ src/app/account/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { AccountService } from '../../account.service';
+declare var $: any;
 
 @Component({
   selector: 'app-login',
